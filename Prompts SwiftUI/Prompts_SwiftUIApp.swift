de2a084bed018a5823560e1bb2327fbe83f025b5
Vishal Dubey <vishal@ivella.com>
2022-12-02 00:43:47 -0800

    init

---
+++ Prompts SwiftUI/Prompts_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Prompts_SwiftUIApp.swift
+//  Prompts SwiftUI
+//
+//  Created by Vishal Dubey on 12/2/22.
+//
+
+import SwiftUI
+
+@main
+struct Prompts_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
